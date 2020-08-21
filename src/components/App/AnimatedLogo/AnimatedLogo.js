9be14949b1048957afaa4b0578e3c1b44b284365
Bertrand Choubert <bertrand.choubert@hotmail.fr>
2020-08-21 11:39:33 +0200

    f/39: Logo Animation (#96)
    - Animated with CSS animations
    - Put it in the DevDoc and in the Splash components

---
+++ src/components/App/AnimatedLogo/AnimatedLogo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { ReactComponent as LogTrackLogo } from './../../../assets/logtrack-alt.svg';
+
+import './AnimatedLogo.scss';
+
+const AnimatedLogo = () => {
+
+  return <div className="AnimatedLogo">
+    <LogTrackLogo />
+  </div>;
+};
+
+export default AnimatedLogo;
